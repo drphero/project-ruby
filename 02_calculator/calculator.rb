359dbca8d24540eb44413da348a01f28a39e4be0
Brent Benofsky <bbenofsky@gmail.com>
2015-09-15 15:19:13 +0000

    update calculator.rb and calculator_spec.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -8,4 +8,20 @@ end
 
 def sum(numbers)
   numbers.inject(0) {|sum, n| sum + n}
+end
+
+def multiply(*numbers)
+  numbers.inject(1) {|product, n| product * n}
+end
+
+def power(num1,num2)
+  num1 ** num2
+end
+
+def factorial(number)
+  if number == 0
+    return 1
+  else
+  (1..number).inject(:*)
   end
+end
